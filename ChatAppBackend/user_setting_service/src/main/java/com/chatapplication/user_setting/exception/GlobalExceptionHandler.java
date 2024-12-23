f2c1a0d2b99c1c6217fe5442128c46dbdf99e444
Abhinav <abhinavmshr96@gmail.com>
2024-12-23 13:40:34 +0530

    created globalExceptionHandler class - no method added

---
+++ ChatAppBackend/user_setting_service/src/main/java/com/chatapplication/user_setting/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.chatapplication.user_setting.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
