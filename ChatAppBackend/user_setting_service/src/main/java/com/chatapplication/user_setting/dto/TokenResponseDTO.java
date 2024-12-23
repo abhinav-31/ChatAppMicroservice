1c99b64597afbc98258bd7b42548806fbb375339
Abhinav <abhinavmshr96@gmail.com>
2024-12-23 13:40:02 +0530

    tokenResponseDTO added

---
+++ ChatAppBackend/user_setting_service/src/main/java/com/chatapplication/user_setting/dto/TokenResponseDTO.java
@@ -0,0 +1,14 @@
+package com.chatapplication.user_setting.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenResponseDTO {
+    private String accessToken;
+}
