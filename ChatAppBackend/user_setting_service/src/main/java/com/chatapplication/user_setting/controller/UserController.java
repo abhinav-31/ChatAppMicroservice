68a2ac99915e0851022d3c122916b040bd767fec
Abhinav <abhinavmshr96@gmail.com>
2024-12-23 13:15:56 +0530

    created usercontroller and added saveUser endpoint

---
+++ ChatAppBackend/user_setting_service/src/main/java/com/chatapplication/user_setting/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.chatapplication.user_setting.controller;
+
+import com.chatapplication.user_setting.dto.SaveUserReqDTO;
+import com.chatapplication.user_setting.service.IUserService;
+import com.chatapplication.user_setting.util.CookieUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    IUserService userService;
+
+    // save user
+    @PostMapping("/save")
+    public ResponseEntity<?> saveUser(@RequestBody SaveUserReqDTO saveUserReqDTO, HttpServletRequest request) {
+        System.out.println("user save api invoke");
+        System.out.println("request:" + request);
+
+        String result = userService.saveUser(saveUserReqDTO,request);
+        // Clear Cookie
+        String clearCookie = CookieUtils.deleteCookie(request,"signUpSessionId");
+        return ResponseEntity.status(HttpStatus.CREATED).header("Set-Cookie",clearCookie).body(result);
+    }
+
+}
