63a94b7ce7d0844635a6653372c55f45cdcd7a6b
Abhinav <abhinavmshr96@gmail.com>
2024-12-23 13:13:21 +0530

    initialzing twilio at the time of startup

--- ChatAppBackend/user_setting_service/src/main/java/com/chatapplication/user_setting/UserSettingServiceApplication.java
+++ ChatAppBackend/user_setting_service/src/main/java/com/chatapplication/user_setting/UserSettingServiceApplication.java
@@ -1,10 +1,24 @@
 package com.chatapplication.user_setting;
 
+import com.chatapplication.user_setting.config.TwilioConfig;
+import com.twilio.Twilio;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class UserSettingServiceApplication {
+
+	@Autowired
+	private TwilioConfig twilioConfig;
+
+	@PostConstruct
+	public void intiTwilio(){
+		Twilio.init(twilioConfig.getAccountSid(),twilioConfig.getAuthToken());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserSettingServiceApplication.class, args);
