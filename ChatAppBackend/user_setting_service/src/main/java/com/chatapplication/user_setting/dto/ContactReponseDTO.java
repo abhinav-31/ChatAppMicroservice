b56de811a96ec149cfb0d539f70cc17438ab524d
Abhinav <abhinavmshr96@gmail.com>
2024-12-24 21:03:53 +0530

    added contact response dto

---
+++ ChatAppBackend/user_setting_service/src/main/java/com/chatapplication/user_setting/dto/ContactReponseDTO.java
@@ -0,0 +1,17 @@
+package com.chatapplication.user_setting.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContactReponseDTO {
+    private String username;
+    private String phoneNumber;
+    private String about;
+    private String profilePhoto;
+}
