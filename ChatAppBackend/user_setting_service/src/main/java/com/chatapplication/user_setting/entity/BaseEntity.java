dbc30b69c02bb7f5d0ef659b52699c299f1ca70e
Abhinav <abhinavmshr96@gmail.com>
2024-12-24 21:00:35 +0530

    added getter to get Id

--- ChatAppBackend/user_setting_service/src/main/java/com/chatapplication/user_setting/entity/BaseEntity.java
+++ ChatAppBackend/user_setting_service/src/main/java/com/chatapplication/user_setting/entity/BaseEntity.java
@@ -1,11 +1,15 @@
 package com.chatapplication.user_setting.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 public class BaseEntity {
     @Id
