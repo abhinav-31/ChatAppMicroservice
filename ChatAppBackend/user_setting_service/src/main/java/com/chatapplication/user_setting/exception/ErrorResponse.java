491f6f6ae2066c690a12834cf87a968e36aa45e4
Abhinav <abhinavmshr96@gmail.com>
2024-12-24 16:50:53 +0530

    ErrorResponse class for exception in the app added

---
+++ ChatAppBackend/user_setting_service/src/main/java/com/chatapplication/user_setting/exception/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.chatapplication.user_setting.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String error;
+    private String message;
+}
