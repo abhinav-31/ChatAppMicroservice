7c66f9e5362c5ffbca8c125c9968a1a45b163c71
Abhinav <abhinavmshr96@gmail.com>
2024-12-24 18:57:52 +0530

    added logging, default settings for the user during user save

--- ChatAppBackend/user_setting_service/src/main/java/com/chatapplication/user_setting/service/UserServiceImpl.java
+++ ChatAppBackend/user_setting_service/src/main/java/com/chatapplication/user_setting/service/UserServiceImpl.java
@@ -1,22 +1,22 @@
 package com.chatapplication.user_setting.service;
 
 import com.chatapplication.user_setting.dto.SaveUserReqDTO;
+import com.chatapplication.user_setting.entity.Settings;
 import com.chatapplication.user_setting.entity.User;
 import com.chatapplication.user_setting.exception.ResourceNotFoundException;
 import com.chatapplication.user_setting.repository.UserRepository;
 import com.chatapplication.user_setting.util.CookieUtils;
-import jakarta.servlet.http.Cookie;
+import com.chatapplication.user_setting.util.SettingsUtils;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.crypto.SecretKey;
-import java.util.Arrays;
-import java.util.Base64;
+import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
 @Transactional
 public class UserServiceImpl implements IUserService{
@@ -34,13 +34,19 @@ public class UserServiceImpl implements IUserService{
         String redisKey = "signup:verified:"+uuid;
         Map<String,Object> retrieveData = (Map<String, Object>) redisTemplate.opsForValue().get(redisKey);
         // after retrieving data from the redis delete the session from redis
+        log.info("Deleting Redis session key: {}", redisKey);
         redisTemplate.delete(redisKey);
 
         if (retrieveData == null) {
+            log.error("Verification data not found in Redis for UUID: {}", uuid);
             throw new ResourceNotFoundException("Verification data not found");
         }
         String email = (String)retrieveData.get("email");
         String phoneNumber = (String)retrieveData.get("phoneNumber");
+
+        // Create a default setting for the given user
+        Settings defaultSettings = new Settings();
+        defaultSettings.setSettingsMap(new HashMap<>(SettingsUtils.DEFAULT_SETTINGS));
         // Create new User entity
         User user = User.builder()
                 .email(email)
@@ -48,9 +54,10 @@ public class UserServiceImpl implements IUserService{
                 .about(saveUserReqDTO.getAbout())
                 .username(saveUserReqDTO.getUsername())
                 .profilePhoto(saveUserReqDTO.getProfilePicture())
+                .settings(defaultSettings)
                 .build();
+        // save user in the repository
         userRepository.save(user);
-
-        return "User saved successfully";
+        return "User saved successfully with default settings";
     }
 }
