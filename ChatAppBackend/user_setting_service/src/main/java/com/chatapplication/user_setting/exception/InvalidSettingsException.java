cc270f8cf933ad51a3c67fcf66aa47c1485c7199
Abhinav <abhinavmshr96@gmail.com>
2024-12-24 18:59:59 +0530

    added custom exception for settings

---
+++ ChatAppBackend/user_setting_service/src/main/java/com/chatapplication/user_setting/exception/InvalidSettingsException.java
@@ -0,0 +1,7 @@
+package com.chatapplication.user_setting.exception;
+
+public class InvalidSettingsException extends RuntimeException {
+    public InvalidSettingsException(String message) {
+        super(message);
+    }
+}
