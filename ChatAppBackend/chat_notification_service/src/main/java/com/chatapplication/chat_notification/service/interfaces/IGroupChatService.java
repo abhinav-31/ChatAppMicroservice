f65e3495d207df8ea7c6e69a99eb317b044c86b0
Abhinav <abhinavmshr96@gmail.com>
2025-01-31 14:49:39 +0530

    added Service Interface: GroupChat

---
+++ ChatAppBackend/chat_notification_service/src/main/java/com/chatapplication/chat_notification/service/interfaces/IGroupChatService.java
@@ -0,0 +1,19 @@
+package com.chatapplication.chat_notification.service.interfaces;
+
+import com.chatapplication.chat_notification.dto.*;
+
+import java.util.List;
+
+public interface IGroupChatService {
+    GroupChatCreateResDTO createGroupChat(GroupChatCreateReqDTO group);
+
+    Boolean addMembersToGroup(GroupChatAddMemReqDTO groupChatAddMemReqDTO);
+
+    Boolean removeMembersFromGroup(GroupChatRemoReqDTO groupChatRemoReqDTO);
+
+    void deleteGroupChatInstanceByMembers(DeleteGroupInstByMemDTO deleteGroupInstByMemDTO);
+
+    List<ChatResponseDTO> getGroupChatForUsers(String userId);
+
+    String deleteGroup(String groupId);
+}
