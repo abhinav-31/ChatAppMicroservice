a2b2c58c2f63eb6c1545efdd29b2effea1f76471
Abhinav <abhinavmshr96@gmail.com>
2025-01-31 14:36:18 +0530

    added scheduling annotation, configure ModelMapper and ObjectMapper Bean

--- ChatAppBackend/chat_notification_service/src/main/java/com/chatapplication/chat_notification/ChatNotificationServiceApplication.java
+++ ChatAppBackend/chat_notification_service/src/main/java/com/chatapplication/chat_notification/ChatNotificationServiceApplication.java
@@ -1,13 +1,42 @@
 package com.chatapplication.chat_notification;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.modelmapper.AbstractConverter;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.Module;
+import org.modelmapper.convention.MatchingStrategies;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.time.LocalDateTime;
+
+@EnableScheduling
 @SpringBootApplication
 public class ChatNotificationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatNotificationServiceApplication.class, args);
 	}
+	@Bean
+	public ModelMapper mapper(){
+		ModelMapper modelMapper = new ModelMapper();
 
+		modelMapper.getConfiguration()
+				.setAmbiguityIgnored(true)
+				.setMatchingStrategy(MatchingStrategies.LOOSE);
+		return new ModelMapper();
+	}
+	@Bean
+    public ObjectMapper objectMapper(){
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		return objectMapper;
+	}
 }
+
+
